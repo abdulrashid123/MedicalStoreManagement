3379d6e381eaf6512f98ec9bf7be11282dce500e
AbdulRashid <abdulrashidalaskar@gmail.com>
2021-09-26 18:58:16 +0530

    first commit

--- MedicalStoreManagement/settings.py
+++ MedicalStoreManagement/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken'
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -52,7 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
 
 ROOT_URLCONF = 'MedicalStoreManagement.urls'
 
