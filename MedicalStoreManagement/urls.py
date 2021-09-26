0fae5e717f96282166949fbec9c1dc4640ffe4bf
AbdulRashid <abdulrashidalaskar@gmail.com>
2021-09-26 11:24:40 +0530

    first commit

--- MedicalStoreManagement/urls.py
+++ MedicalStoreManagement/urls.py
@@ -1,7 +1,7 @@
-"""MedicalStoreManagement URL Configuration
+"""MedicalStoreManagment URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+from customer.views import Sample
+from rest_framework.authtoken import views
+from customer.views import SearchMedicine,TokenView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth/', TokenView.as_view()),
+    path('',include(('customer.urls','customer'),namespace='customer')),
+    path('',include(('purchase.urls','purchase'),namespace='purchase')),
+    path('sample/',Sample.as_view()),
+    path('search/',SearchMedicine.as_view())
 ]
